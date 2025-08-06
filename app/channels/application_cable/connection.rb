078c947c04dd917fc1568cbf9bcb94af44f02fa4
Neil Northrop <nnorthrop@gmail.com>
2025-08-06 10:14:10 -0700

    add action cable

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,21 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:token]
+      decoded_token = JsonWebToken.decode(token)
+      if (current_user = User.find(decoded_token[:user_id]))
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
+end
--- app/services/action_service.rb
+++ app/services/action_service.rb
@@ -21,6 +21,7 @@ class ActionService
       user_action.update(last_performed_at: Time.current)
       @user.gain_experience(10)
       @user.save
+      UserUpdatesChannel.broadcast_to(@user, { type: 'user_action_update', data: UserActionSerializer.new(user_action, include: [:action]).serializable_hash })
       { success: true, message: "#{action.name} performed successfully." }
     else
       { success: false, error: 'Action is on cooldown.' }
