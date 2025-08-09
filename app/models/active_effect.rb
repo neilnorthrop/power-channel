2b783b99cef9592de53677e66f6fdd4a70ae6411
Neil Northrop <nnorthrop@gmail.com>
2025-08-09 07:35:24 -0700

    fix rubocop errors

--- app/models/active_effect.rb
+++ app/models/active_effect.rb
@@ -2,5 +2,5 @@ class ActiveEffect < ApplicationRecord
   belongs_to :user
   belongs_to :effect
 
-  scope :active, -> { where('expires_at > ?', Time.current) }
+  scope :active, -> { where("expires_at > ?", Time.current) }
 end
